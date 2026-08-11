cde47897cba7635db77bab041a2e24994f2235f5
hzk <859436774@qq.com>
2026-08-11 17:03:24 +0800

    优化右上角的东西

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -9,7 +9,5 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 export default defineNavbarConfig([
   { text: '首页', link: '/' },
   { text: '博客', link: '/blog/' },
-  { text: '标签', link: '/blog/tags/' },
-  { text: '归档', link: '/blog/archives/' },
   { text: '手册', link: '/reference/' },
 ])
